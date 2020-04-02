80e2fcdbad7534742424d213d2bd15277289c853
davet <daveenq@gmail.com>
2020-04-02 04:13:35 +0100

    Added create-react-app application and tidied  up code

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   React
+    </div>
+  );
+}
+
+export default App;
